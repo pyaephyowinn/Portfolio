7cbdee5cf032a56d905781d184b571e59da20200
pyaephyowin <pyaephyowinnn@gmail.com>
2022-08-09 14:03:33 +0630

    add not found page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./routes/Home";
 import Contact from "./routes/Contact";
+import NotFound from "./routes/NotFound";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
---
+++ src/routes/NotFound/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+import styles from "./index.module.css"
+
+const NotFound = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Page is not found</h1>
+      <p>Go to <Link to={"/"}>Home</Link></p>
+    </div>
+  )
+}
+export default NotFound
